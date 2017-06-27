68c4325a0634dd0bb99c5b692580ff850ec9d217
booox <booox@users.noreply.github.com>
2017-06-28 06:35:20 +0800

    done devise can login with username or email

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,28 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :authentication_keys => [:login]
+
+  attr_accessor :login
 
   has_one :registration
   has_one :profile
   has_many :kins
 
 
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    else
+      if conditions[:username].nil?
+        where(conditions).first
+      else
+        where(username: conditions[:username]).first
+      end
+    end
+  end
 
   def admin?
     is_admin
