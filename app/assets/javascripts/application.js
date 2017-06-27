42c52c013ebdfeacc811b6a51781e0e40ba1e7a2
booox <booox@users.noreply.github.com>
2017-06-27 14:07:02 +0800

    implement mobile response meta

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,6 +15,7 @@
 //= require turbolinks
 //= require bootstrap/alert
 //= require bootstrap/dropdown
+//= require bootstrap/collapse
 //= require bootstrap-datepicker/core
 //= require bootstrap-datepicker/locales/bootstrap-datepicker.zh-CN
 //= require nested_form_fields
